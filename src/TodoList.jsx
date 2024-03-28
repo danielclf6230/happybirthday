82fcaf2483421790ac1de1682c71b2608b5ef63f
Ling Fung Chow <133887948+danielclf6230@users.noreply.github.com>
2024-03-27 21:34:47 -0600

    changed name

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,7 +3,7 @@ import { TodoItem } from "./TodoItem";
 export function TodoList({ todos, toggleTodo, deleteTodo }) {
   return (
     <ul className="list">
-      {todos.length === 0 && "Not Todos"}
+      {todos.length === 0 && "No Items"}
       {todos.map((todo) => {
         return (
           <TodoItem
